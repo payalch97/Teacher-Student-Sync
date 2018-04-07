b570d7276b60ff79bed62f03ee087cddb24532a7
Payal <payalch97@gmail.com>
2018-04-07 22:10:13 +0530

    final commit

--- Program.c
+++ Program.c
@@ -5,13 +5,22 @@
 pthread_mutex_t mut1;
 int i,resource;
 int active[5];
+char* fun1(int x)
+{
+    switch(x)
+    {
+        case 1 : return "Pen";
+        case 2 : return "Paper";
+        case 3 : return"Question Paper";
+    }
+}
 void* teacher(void *c)
 {
     sleep(1);
     while(1)
     {
         pthread_mutex_lock(&mut1);
-        resource = random() % 3;
+        resource = (random() % 1029)%3;
         resource += 1;
         if(active[1]==0 && active[2]==0 && active[3]==0)
             {
@@ -40,15 +49,17 @@ void* teacher(void *c)
 
 void* stud(void *x)
 {
-
-    printf("Student is ready for Test having %s\n");
+    int studentID = *(int *)x;
+    printf("Student %d is ready for Test having %s\n",studentID,fun1(studentID));
     while(1)
     {
         pthread_mutex_lock(&mut1);
         if(studentID == resource)
         {
+            printf("Student %d already have %s\n",studentID,fun1(studentID));
+            printf("Student %d has acquired the resource available on desk \n",studentID);
             active[studentID]=0;
-            printf("Student completed the Test \n");
+            printf("Student %d completed the Test \n",studentID);
             pthread_mutex_unlock(&mut1);
             break;
         }
@@ -59,15 +70,19 @@ void* stud(void *x)
 
 void main()
 {
-    pthread_t t1,s1,s2,s3;
+    pthread_t t1;
+    pthread_t *t = malloc(sizeof(pthread_t)*3);
+    int *x = malloc(sizeof(int)*3);
+    pthread_mutex_init(&mut1,NULL);
+    pthread_create(&t1,NULL,teacher,NULL);
+    for(i=0;i<3;i++)
+    {
+        *(x+i) = i+1;
+        active[i+1]=1;
+        pthread_create((t+i),NULL,stud,(void*)(x+i));
 
-    pthread_create(t1,NULL,teacher,NULL);
-
-    pthread_create(s1,NULL,stud,NULL);
-    pthread_create(s2,NULL,stud,NULL);
-    pthread_create(s3,NULL,stud,NULL);
+    }
     pthread_join(t1,NULL);
-    pthread_join(s1,NULL);
-    pthread_join(s2,NULL);
-    pthread_join(s3,NULL);
+    for(i=0;i<3;i++)
+        pthread_join(*(t+i),NULL);
 }
